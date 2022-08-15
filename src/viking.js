13db4dc6f8cb4d215a7ac598bbffb01fbc1a18f3
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-15 14:33:39 -0400

    soldier e viking

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health -= damage
+        
+      
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health,strength)
+        this.name = name
+    }
+    receiveDamage(damage){
+        this.health -= damage
+        return this.health
+        if(this.health === 0){
+            return `${name} has received ${damage} points of damage`
+        } else{
+            return `${name} has died in act of combat`
+        }
+        
+
+        
+        
+
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+}
 
 // War
 class War {}
